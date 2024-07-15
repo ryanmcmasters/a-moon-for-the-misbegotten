f5c5e6e8da406195adf532ce51db049aff59cbb4
ryanmcmasters <ryan.mcmasters@gmail.com>
2024-07-15 14:51:58 -0400

    Add files via upload

---
+++ moon_002.v4.rb
@@ -0,0 +1,103 @@
+# moon_interludes_002
+# --a moon rises for the lonely--
+
+# R.McMASTERS
+
+# for quantum theatre's 'a moon for the misbegotten'
+# cody spellman, director
+
+
+#--------setup--------
+# port name
+port = "iac_driver_bus_1"
+
+# midi clear
+midi_all_notes_off port: port, channel: [1,2,3,4,5]
+
+# tempo control
+use_bpm 45
+
+#rotational arrays
+ra = [
+  [0,4,7,9],
+  [0,2,4,6,7],
+  [0,2,4,5,7],
+  [0,2,3,5,7],
+  [0,3,5,7],
+  [0,3,6,8],
+  [0,2,3,7],
+]
+
+
+#--------the score--------
+
+#array controller
+live_loop :control do
+  
+  $it = 0
+  
+  ra.size.times do
+    
+    sleep [4,8,16].choose
+    $it = $it + 1
+    
+  end
+end
+
+
+live_loop :chords do
+  
+  p = ra[$it].tick
+  s = [0.25,0.5,1]
+  
+  rrand_i(2,6).times do
+    
+    midi p + 50 + [0,12,24].choose, sustain: s.choose*2, channel: 1, port: port
+    sleep s.choose
+    
+  end
+end
+
+
+live_loop :chords2 do
+  
+  p = ra[$it].tick
+  s = [0.25,0.5,1]
+  
+  rrand_i(2,6).times do
+    
+    midi p + 50 + [0,12,24].choose, sustain: s.choose*2, channel: 2, port: port
+    sleep s.choose
+    
+  end
+end
+
+
+live_loop :chords3 do
+  
+  p = ra[$it].tick
+  s = [1,1.25,1.5]
+  
+  rrand_i(2,6).times do
+    
+    midi p + 50 + [0,12].choose, sustain: s.choose*2, channel: 3, port: port
+    sleep s.choose
+    
+  end
+end
+
+
+live_loop :cello do
+  
+  midi ra[$it].choose + 50, sustain: 3, channel: 4, port: port
+  sleep 3
+  
+end
+
+
+live_loop :bass do
+  
+  midi ra[$it].choose + 38, sustain: 4, channel: 5, port: port
+  sleep 4
+  
+end
---
+++ moon_004.v4.rb
@@ -0,0 +1,202 @@
+# moon_interludes_004
+# --the evocation of angels and demons--
+
+# R.McMASTERS
+
+# for quantum theatre's 'a moon for the misbegotten'
+# cody spellman, director
+
+
+#--------setup--------
+# port name
+port = "iac_driver_bus_1"
+
+# midi clear
+midi_all_notes_off port: port, channel: [1,2,3,4,5]
+
+# tempo control
+use_bpm 60
+
+# melody array
+mel = [
+  [74,79,76],
+  [74,79,76,81],
+  [72,69,70],
+  [68,69]
+]
+
+
+#--------the score--------
+live_loop :mel do
+  
+  $it = 0
+  s = 1
+  oct = [0,12,24]
+  
+  mel.size.times do
+    
+    p=0
+    
+    mel[$it].size.times do
+      
+      midi mel[$it][p],
+        sustain: s,
+        channel: 3,
+        port: port
+      
+      # harmony conditions
+      if mel[$it][p] == 74
+        
+        midi 57 + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi [66,65].choose + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 79
+        
+        midi [66,65].choose + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi 69 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 78
+        
+        midi 69 + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi 62 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 81
+        
+        midi 74 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        midi [66,65].choose + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 72
+        
+        midi 69 + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi 62 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 69
+        
+        midi [66,65].choose + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi 62 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 70
+        
+        midi 69 + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi 62 + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+        
+      elsif mel[$it][p] == 68
+        
+        midi 57 + oct.choose,
+          channel: 2,
+          sustain: s+2,
+          port: port
+        midi [54,53].choose + oct.choose,
+          channel: 1,
+          sustain: s+2,
+          port: port
+      end
+      
+      sleep 1
+      p=p+1
+      
+    end
+    
+    sleep 3
+    $it = $it +1
+    
+  end
+  
+  sleep rrand_i(2,4)
+  
+end
+
+
+live_loop :cello do
+  
+  use_bpm [90, 120, 150, 180].choose
+  
+  p = [(chord :d2, :major, num_octaves: 2),
+       (chord :d2, :minor, num_octave: 2)].choose.choose
+  
+  rrand_i(1,4).times do
+    midi p,
+      sustain: 1.25,
+      channel: 5,
+      port: port
+    
+    sleep 0.5
+    
+    midi p,
+      sustain: 1.25,
+      vel: 100,
+      channel: 5,
+      port: port
+    
+    sleep 1.5
+    
+  end
+end
+
+
+live_loop :bass do
+  use_bpm [90,120,150,180].choose
+  p = [(chord :d2, :major, num_octaves: 2),
+       (chord :d2, :minor, num_octave: 2)].choose.choose
+  
+  rrand_i(1,4).times do
+    
+    midi p,
+      sustain: 1,
+      channel: 4,
+      port: port
+    
+    sleep 0.5
+    
+    midi p + 12,
+      sustain: 1,
+      vel: 100,
+      channel: 4,
+      port: port
+    
+    sleep 1.5
+    
+  end
+end
